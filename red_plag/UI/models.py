48bc2b1f38245cbbb7c1ac0da6b25e467f764973
Pranay Varma Sunkari <pranay.sunkari15306@gmail.com>
2020-12-05 18:02:15 +0530

    Update models.py

--- red_plag/UI/models.py
+++ red_plag/UI/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 import os
 from django import forms
 #new_group, created = Group.objects.get_or_create(name=group_name)
@@ -15,7 +15,7 @@ class Profile(models.Model):
 class UserModel(models.Model):
    username = models.CharField(max_length=100)
    #password=forms.CharField(max_length=100)
-   email=models.EmailField(max_length=100, default="abcd@abc.com")
+   email=models.EmailField(max_length=100, default="temp@user.com")
    university = models.CharField(max_length=100)
    uploads=models.CharField(max_length=100, default="")
    last_login=models.DateTimeField(auto_now=True)
@@ -25,14 +25,16 @@ class UserModel(models.Model):
    
    class Meta:
       db_table = "usersinfo"
-#class UniversityModel(models.Model):
-#  username = models.CharField(max_length=100)
-#  university = models.CharField(max_length=100)
-#  email = models.EmailField(max_length=100)
-#  passcode = models.CharField(max_length=100)
-#  def __str__(self):
-#      return self.username
-#   
-#  class Meta:
-#      db_table = "univsinfo"
+class UniversityModel(models.Model):
+  username = models.CharField(max_length=100)
+  university = models.CharField(max_length=100)
+  email = models.EmailField(max_length=100, default="temp@univ.con")
+  uploads = models.CharField(max_length=100, default="-1")
+  last_login = models.DateTimeField(auto_now=True)
+  passcode = models.IntegerField(default=000000)
+  def __str__(self):
+      return self.username
+   
+  class Meta:
+      db_table = "univsinfo"
 
