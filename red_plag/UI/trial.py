466c44a818944d5275c1b9711bc77dd9a5d0893b
P Veda <vedapranav@cse.iitb.ac.in>
2020-12-06 01:47:22 +0530

    update

--- red_plag/UI/trial.py
+++ red_plag/UI/trial.py
@@ -5,49 +5,54 @@ import zipfile
 import csv
 from os import listdir
 from os.path import isfile, join
-if(len(sys.argv)>1):
-    with zipfile.ZipFile(sys.argv[1], 'r') as zip_ref:
+def unzip(fil):
+    with zipfile.ZipFile(fil, 'r') as zip_ref:
         zip_ref.extractall('.')
-    files = [join(sys.argv[1].__str__()[0:len(sys.argv[1].__str__())-4], f) for f in listdir(sys.argv[1].__str__()[0:len(sys.argv[1].__str__())-4]) if isfile(join(sys.argv[1].__str__()[0:len(sys.argv[1].__str__())-4], f))]
+    files = [join(fil.__str__()[0:len(fil.__str__())-4], f) for f in listdir(fil.__str__()[0:len(fil.__str__())-4]) if isfile(join(fil.__str__()[0:len(fil.__str__())-4], f))]
+    return files
 
-lines = []
-word_count_vector = []
-lengths = []
+#lines = []
 def merge(l):
    ans = ""
    for i in l:
       ans = ans + i
    return ans.replace('\n', ' ')
-for file in files:
-    with open(file, 'r') as f:
+
+def eliminate_comments(dat):
+    for i in range(len(dat)):
+        ind = dat[i].find("#")
+        #print(ind)
+        if (ind != -1) :
+            dat[i] = dat[i][0:ind] + "\n"
+    return dat
+def find_signature(files):
+    word_count_vector = []
+    lengths = []
+    for file in files:
+        with open(file, 'r') as f:
         #data = f.read().replace('\n', ' ')
         #print(data)
-        dat = f.readlines()
+            dat = f.readlines()
         #print(dat)
-        for i in range(len(dat)):
-            ind = dat[i].find("#")
-            #print(ind)
-            if (ind != -1) :
-               dat[i] = dat[i][0:ind] + "\n"
+            dat = eliminate_comments(dat)
             #print(l)
         #print(dat)
-        data = merge(dat)
+            data = merge(dat)
         #print(data)
-        data = re.sub(r'[^\w]', ' ', data)
-        lines.append(data)
-        words = data.split(' ')
-        words_unique = list(np.unique(np.array(words)))
-        freq = {w : 0 for w in words_unique}
-        for word in words:
-            freq[word] = freq[word] + 1
-        freq.pop('')
-        word_count_vector.append(list(freq.values()))
-        lengths.append(len(freq))
+            data = re.sub(r'[^\w]', ' ', data)
+        #lines.append(data)
+            words = data.split(' ')
+            words_unique = list(np.unique(np.array(words)))
+            freq = {w : 0 for w in words_unique}
+            for word in words:
+                freq[word] = freq[word] + 1
+            freq.pop('')
+            word_count_vector.append(list(freq.values()))
+            lengths.append(len(freq))
+    return [word_count_vector, lengths]
 #print(word_count_vector)
 #print(lengths)
-if(len(sys.argv)<=1):
-    print("ADD ARGUMENTS")
-if(len(lengths)!=0):
+def sort_pad(lengths, word_count_vector):
     final_length = max(lengths)
     for w in word_count_vector:
         if (len(w) == final_length):
@@ -58,8 +63,10 @@ if(len(lengths)!=0):
             for i in range(num):
                 w.append(0)
             w.sort()
+    return [word_count_vector, final_length]
 
 #print(word_count_vector)
+def normalize(word_count_vector, final_length):
     final_word_count = [[] for j in range(final_length)]
 
     for w in word_count_vector:
@@ -75,45 +82,36 @@ if(len(lengths)!=0):
             if (stds[i] == 0):
                 continue
             w[i] = (w[i] - means[i])/stds[i]
-
+    return word_count_vector
 #print(word_count_vector)
+def similar(word_count_vector):
     an = [0 for i in range(len(word_count_vector))]
     ad = [an for i in range(len(word_count_vector))]
     final = np.array(ad, dtype=float)
     for i in range(len(word_count_vector)):
         for j in range(len(word_count_vector)):
-            if((np.linalg.norm(np.array(word_count_vector[i]))*np.linalg.norm(np.array(word_count_vector[j])))!=0):
-                val = np.dot(np.array(word_count_vector[i]), np.array(word_count_vector[j])) / (np.linalg.norm(np.array(word_count_vector[i]))*np.linalg.norm(np.array(word_count_vector[j])))
-            if((np.linalg.norm(np.array(word_count_vector[i]))*np.linalg.norm(np.array(word_count_vector[j])))==0):
-                val = 1
+            val = np.dot(np.array(word_count_vector[i]), np.array(word_count_vector[j])) / (np.linalg.norm(np.array(word_count_vector[i]))*np.linalg.norm(np.array(word_count_vector[j])))
             final[i][j] = val
-print(final)
-final = final.astype('str')
+    return final
+
+def evaluate(zip):
+    files = unzip(zip)
+    [word_count_vector, lengths] = find_signature(files)
+    [word_count_vector, final_length] = sort_pad(lengths, word_count_vector)
+    word_count_vector = normalize(word_count_vector, final_length)
+    final = similar(word_count_vector)
+    print(final)
+    return final
 #print(np.dot(np.array(word_count_vector[0]), np.array(word_count_vector[0])) / (np.linalg.norm(np.array(word_count_vector[0]))*np.linalg.norm(np.array(word_count_vector[0]))))
-file = open('comparision.csv', 'wb')
-file1 = open('comparision.csv', 'a+', newline ='')
-l = [i for i in range(0,len(lengths)+1)]
-for i in range(0, len(l)):
-    if(i==0):
-        l[i] = 'files'
-    if(i!=0): 
-        l[i] = 'w.r.t file'+str(l[i])
-arr = [i for i in range(1,len(lengths)+1)]
-for i in range(0,len(arr)):
-    arr[i] = 'file' + str(arr[i])
-print(arr)
-arr = np.array(arr)
-result = np.hstack((final, np.atleast_2d(arr).T))
-for i in range(len(lengths)):
-    result[:, [len(lengths)-i, len(lengths)-i-1]] = result[:, [len(lengths)-i-1, len(lengths)-i]]
-print(result) 
+final = evaluate(sys.argv[1])
+def csv_write(final):
+    file = open('comparision.csv', 'wb')
+    file1 = open('comparision.csv', 'a+', newline ='')
+
 # writing the data into the file
-i=0
-with file1:
-    if(i==0):
-        write = csv.writer(file1) 
-        write.writerow(l) 
-        i=1
-    if(i!=0):        
-        write = csv.writer(file1) 
-        write.writerows(result)
+    with file1:
+        write = csv.writer(file1)
+        write.writerows(final)
+
+csv_write(final)
+
